0c03b6c55c1312d9c345f49a5a9949ff4852150d
Morgan <morganjweaver@gmail.com>
2015-02-01 22:46:37 -0800

    Fixed input parser, debugged move command, added pretty_print for canonical ---> user text conversion

--- feline_war_data.js
+++ feline_war_data.js
@@ -1,35 +1,38 @@
-var places = {
-  Alleyway: {
+feline_war = {
+places: {
+  alleyway: {
     description: 'You find yourself in a filthy alleyway of scorched concrete and bones.  A dumpster sits to one side.',
-    next_places: ['Spacecraft', 'Chinchilla Mart', 'Bomb Shelter' ],
+    next_places: ['spacecraft', 'chinchilla_mart', 'bomb_shelter' ],
+
   },
-  Bomb_Shelter: {
+  bomb_shelter: {
     description: 'You are now standing in a shabby concrete bunker.  There is a table with a couple of splintered chairs, a couple of cabinets, and. . . what\'s this. . . a trap door?',
-    next_places: ['Alleyway', 'War Room'],
+    next_places: ['alleyway', 'war_room'],
   },
-  War_Room: {
+  war_room: {
     description: 'You are in a tiny secret room, which appears to have been used for planning attack strategies.  It is filled with cobwebs, and a heap of trash in one corner.',
-    next_places: ['Bomb Shelter'],
+    next_places: ['bomb_shelter'],
   },
-  Spacecraft: {
+  spacecraft: {
     description: 'You now stand inside a downed ship.  Twisted metal and broken wires are everywhere.  An anti-cat propaganda poster is still clinging to the wall.',
-    next_places: ['Sick Bay', 'Alleyway'],
+    next_places: ['sick_bay', 'alleyway'],
   },
-  Sick_Bay: {
+  sick_bay: {
     description: 'You enter the ship\'s sick bay.  Cryo rehabilitation gear and an antigravity operating bed cram the tiny space.',
-    next_places: ['Spacecraft'],
+    next_places: ['spacecraft'],
   },
-  Chinchilla_Mart: {
+  chinchilla_mart: {
     description: 'You are inside the remnants of a Chinchilla Mart. The floor is dusted with scraps of neon packaging.  You can almost hear the Chinchilla Mart jingle ringing in your ears.  An information kiosk sits in one corner, luckily powered by its nuclear battery.',
-    next_places: ['Alleyway', 'Kiosk', 'Soylent Refinery'],
+    next_places: ['alleyway', 'kiosk', 'soylent_refinery'],
   },
-  Soylent_Refinery: {
-    description: 'The Soylent refinery has clearly been pillaged of its various nutrition-rich, food-like wafers.  Vats and conveyor belts sit empty, and an eerie smell haunts the large factory floor.',
-    next_places: ['Chinchilla Mart'],
+  soylent_refinery: {
+    description: 'The Soylent refinery has clearly been pillaged of its various nutritionally calibrated, food-like wafer products.  Vats and conveyor belts sit empty, and an eerie smell haunts the large factory floor.',
+    next_places: ['chinchilla_mart'],
   },
-  Kiosk: {
+  kiosk: {
     description: 'You wander over to the battered kiosk.  A screen offers several news articles and what appear to be federally sponsored survival information for megafeline emergencies.  Enter a number to access an article or leave the kiosk.',
-    articles: [
+  next_places: ['chinchilla_mart'],
+  articles: [
       { 
         title: 'Xelebrity News',
         contents: '<Xelebrity News, March 20, 3011, 20:15> Rock star research scientist Kifteq VC25 announces the release of new cerebral nanobot technology enabling direct frontal forebrain/G-wave interface---5th Season of Quantum Scandal initial screening attended by Chinese Royal Family---Nanodrop chanteuse/-teur Rit Kaylor CB99 elects to have second clone---Zero-G talking wombat wins hearts, minds of the nation\'s remaining children </Xelebrity News>',
@@ -47,8 +50,8 @@ var places = {
         contents: '<UNICORP Global Push, November 1, 3011, 12:00> Please enter your language or corperate nomenclature code for non-standard English.  Clowder positively identified as Lasercat threat and confirmed by all major corporate and government intelligences.  Remain in your shelters and do not leave for any reason.  RADIC, UNIC, EURASIA, and ANTARC forces are currently attempting to contain all Lasercats for further research and experimentation. DO NOT open any food rations (dehydrated, tablets, modules or threads) containing liver pate or suspected imitation liver pate flavoring. Shelter guests will be advised to leave upon threat neutralization. </UNICORP Global Push>',
       },
       {
-        title: "Attack NeuTralIzing Multi-Edible Organic Weaponry (ANTIMEOW)"
-        contents: "1. 	Cat Attack Cookies: A nutrious treat for those special occasions.\n \n"
+        title: "Attack NeuTralIzing Multi-Edible Organic Weaponry (ANTIMEOW)",
+        contents: "1. 	Cat Attack Cookies: A nutrious treat for those special occasions.\n \n" +
 'Ingredients \n' +
 '1 C	Whole brown rice flour \n' +
 '1/4 C	Soy flour \n' +
@@ -76,29 +79,29 @@ var places = {
 '1/4 C	Chopped parsley\n\n' +
 
 'Directions\n' +
-'Combine all ingredients and mix well. Shape into balls of desired size or simply spoon into cat\'s dish and serve. These treats may be stored in the refrigerator for up to three days, and may also be frozen.'
+'Combine all ingredients and mix well. Shape into balls of desired size or simply spoon into cat\'s dish and serve. These treats may be stored in the refrigerator for up to three days, and may also be frozen.',
       },
-    ]
-  }
-}
- 
-var laser_cats = {
+    ],
+  },
+},
+laser_cats: {
   name: ['Mr. Whiskas', 'Peaches','Dr. Meow', 'Spicy Bob',],
-  color:['Calico', 'Black', 'Extra Fluffy White', 'Purple',],
-  rank:['Kitten', 'Junior Cadet', 'Shock Troop', 'Covert Ops',],
-  HP:[2, 5, 7, 10]
-//HP stands for 'Hit Points' or total damage laser cat may sustain
+  color: ['Calico', 'Black', 'Extra Fluffy White', 'Purple',],
+  rank: ['Kitten', 'Junior Cadet', 'Shock Troop', 'Covert Ops',],
+  // HP stands for 'Hit Points' or total damage laser cat may sustain.
+  HP: [2, 5, 7, 10],
+},
+boss_cat: {
+  name: ['Admiral Fluff-Muffin',],
+  color: ['Neon Leapard',],
+  rank: ['Admiral--Use extreme caution!',],
+  HP: [15,],
+},
 }
 
-var boss_cat = {
-  name: 'Admiral Fluff-Muffin',
-  color: 'Neon Leapard',
-  rank: 'Admiral--Use extreme caution!',
-  HP: 15
-}
-
-var items = {
-  //data structure experiment to remove item from inventory after use
+/*
+ items = {
+  // Data structure experiment to remove item from inventory after use
   pelt: { 
     var pelt = 'pelt';
     HP = (HP + 5);
@@ -106,7 +109,7 @@ var items = {
     item_removal(pelt);
     }
   pate_treats: {
-    //end laser cat round/cat runs away
+    // End laser cat round/cat runs away
     console.log('You hurl the goose pate flavored laser treats far off into the distance.  The laser cat rushes away in an unearthly fury to recover the very substance which may have tempted it to Earth. . .';
     slaying = false;
     }
@@ -138,4 +141,4 @@ var item_removal = function('item_name'){
     inventory.splice(i, 1);
     }
   }
-}
+}*/
--- text_engine.js
+++ text_engine.js
@@ -1,7 +1,5 @@
-//make browser commands work in jsc
-load('console_emulation.js')
-
-var test_places = {
+var test_game = {
+places: {
   place1: {
     description: 'yer in place 1',
     next_places: ['place2', 'place3',],
@@ -14,35 +12,164 @@ var test_places = {
     description: 'u r in place3',
     next_places: ['place1', 'place2',],
   }
+},
 };
+  
 
+//
+// Global Variables
+//
 
-//stores player's current place and list of all places
-var places = test_places;
+// stores player's current place and list of all places
 var current_place = {};
+var commands = {};
+var inventory = {};
+var places = {};
+
+
+//
+// Commands for moving to new places, utilizing items, etc.
+//
+
+// pretty_format transforms next_places (in data file) into user-friendly format
+// I.e. cat_party --> Cat Party
+var pretty_format = function(key) {
+  var split_key = key.split("_");
+  var ret = '';
+  for (i in split_key) { // looping over arrays returns indecies
+    var word = split_key[i];
+    ret += word[0].toUpperCase() + word.substring(1, word.lenght) + ' ';
+  }
+  return ret.substring(0, ret.length -1);
+}
+
+var test_pretty_format = function() {
+  console.log('Pretty Format: ' + pretty_format('chic_mart'));
+}
 
 var look = function() {
   console.log(current_place.description);
+  console.log('These are the places you can go next: ' + 
+    current_place.next_places.map(pretty_format));
 }
-//functions for moving to new places, utilizing items, etc.
 
-var goto = function(place) {
+var move = function(place) {
     current_place = places[place];
     look(place);
 }
 
-//not sure how to translate comparison of user input to place object's next possible place list in JS-LINE 18
-var command_goto = function(place) {
+
+
+// display what the user can do
+var help = function() {
+  for ( c in commands ) {
+    console.log(c + " : " + commands[c].help)
+  }
+}  
+// not sure how to translate comparison of user input to place object's next possible place list in JS-LINE 18
+var command_move = function(place) {
     if (current_place.next_places.indexOf(place) == -1) {
         console.log("You can't go there from your current place.  Maybe you can access it from somewhere else?");
     } else {
-        goto(place);
-    }
- 
+        move(place);
+    }  
 };
 
-var inventory = {}
+var command_look = function(place) {
+  look();
+}
 
-var use = function() {
+var command_quit = function(input){
+  quit();
+}
+var command_help = function(input) {
+  help();
+}
+
+var commands = {
+  move : {
+    help : 'move to location specified',
+    command : command_move, 
+  },
+  look : { 
+    help: 'look around the current place and see where you can go next',
+    command : command_look,
+  },
+  quit : {
+    help: 'mission abort--exits game',
+    command : command_quit,
+  },
+  help : {
+    help: 'displays available commands',
+    command: command_help,
+  }
+}
+
+
+//
+// Internal game functions
+//
+
+// Given input, returns an object with a command variable
+// and a arguments variable. The command variable is the first
+// string before a space, the argument is everything outside
+// the space.
+var parse_input = function(input) {
+    //need to make parser separate command and arg, save seperately
+    var lower_input = input.toLowerCase();
+    // Move(Chinchilla Mart) --> move(chinchilla mart)
+    //var user_text_underscored = lower_input.replace(/ /g,"_");
+    //move(chinchilla mart) --> move(chinchilla_mart)
+    // to do: fix parser
+    var user_parsed_command = lower_input.slice(0, lower_input.indexOf(' '));
+    var user_parsed_arg = lower_input.slice(lower_input.indexOf(' ') +1, lower_input.length);
+    user_parsed_arg = user_parsed_arg.replace(/ /g,"_");
+  
+  var ret = {
+    command : user_parsed_command,
+    argument : user_parsed_arg,
+  };
+  console.log(ret.command);
+  console.log(ret.argument);
+  return ret;
+}
+
+
+var run_game = function(game_data, start_place) {
+  places = game_data.places;
+  move(start_place);
+  //maybe substitute player_alive or boss_cat consition for true below?
+  while (true) {
+    input = parse_input(prompt('What do you want to do?'));
+    if (!(input.command in commands)) {
+      console.log('that doesn\'t look like a valid command.');
+      help(); 
+    } else {
+      //finds chosen command in commands list, takes input argument
+      commands[input.command].command(input.argument)
+    }
+  }
+}
+
+
+//is this legit?  add to end of every item script?
+/*var remove_item = function(item) {
+  inventory.replace('item','');
+}
+
+//do I need to specify for use while battling cat?  will the cat fight loop just exclude the possibility of 
+//executing other commands?
+var use = function(item) {
+  if (slaying == false) {
+    console.log('Hm, that didn\'t seem to do much--try using it next time you see a laser cat!');
+    } else {
+//execute code stored in a specific item, apply to only current battle
+//then attempts to remove the item from inventory
+    var index = array.indexOf(item);
+    array.splice(index, 1);
+    }
+}
+*/
+    
     
 
